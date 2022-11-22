d393fbdbf9db72ac395c0c804f6779e2ccd9eff2
parameswararao960 <parama960@gmail.com>
2022-11-22 16:10:23 +0530

    Factorial Program

--- MyPractice/Program.cs
+++ MyPractice/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Hello
 {
     class Program
     {
+
+        public static int RecFac(int a)
+        {
+            return (a==0 ? 1 : a * RecFac(a-1));
+        }
         public static void Main(string[] args)
         {
-
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(RecFac(a));
         }
     }
 }
